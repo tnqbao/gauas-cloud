c9a5ef70d7bb49bbc6062bbcbfc5d5c6c6a487a6
unknown <quocbao.job106204@gmail.com>
2026-01-11 20:58:15 +0700

    [Publish] Add Main Repositories

--- migration/000005_remove_unique_email_constraint.down.sql
+++
@@ -1,9 +0,0 @@
--- Restore UNIQUE constraint on email column
-ALTER TABLE iam_users ADD CONSTRAINT iam_users_email_key UNIQUE (email);
--- Remove UNIQUE constraint from email column in iam_users table
-ALTER TABLE iam_users DROP CONSTRAINT IF EXISTS iam_users_email_key;
-
--- Recreate the index without UNIQUE constraint (keep it for performance)
-DROP INDEX IF EXISTS idx_iam_users_email;
-CREATE INDEX IF NOT EXISTS idx_iam_users_email ON iam_users(email);
-
--- migration/000008_create_upload_sessions_table.down.sql
+++
@@ -1,3 +0,0 @@
--- Drop upload_sessions table
-DROP TABLE IF EXISTS upload_sessions;
-
